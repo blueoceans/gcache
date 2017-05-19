3c9d2989a6c6ca998b6390f80b037b742d72967b
ENDOH takanao <djmchl@gmail.com>
2017-05-20 08:52:50 +1200

    add GetTokenBucketGDriveAPI

--- tokenbucket.go
+++ tokenbucket.go
@@ -13,3 +13,8 @@ var (
 func init() {
 	tokenBucketGDriveAPI = tokenbucket.NewTokenBucket(time.Duration(100)*time.Second, 1000) // 1000/100sec (user limit)
 }
+
+// GetTokenBucketGDriveAPI returns a token-bucket for calling the Google Drive API.
+func GetTokenBucketGDriveAPI() chan tokenbucket.Token {
+	return tokenBucketGDriveAPI
+}
