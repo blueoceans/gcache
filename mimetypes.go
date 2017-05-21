f7798d11e9ce8650637672b7b59aa35f44385eb3
ENDOH takanao <djmchl@gmail.com>
2017-05-21 13:09:43 +1200

    add comment

--- mimetypes.go
+++ mimetypes.go
@@ -3,6 +3,7 @@ package gcache
 const (
 	mimeTxt string = "text/plain"
 
+	// https://developers.google.com/drive/v3/web/mime-types
 	// MimeGSuiteDoc is mime-types of the document on Google Drive.
 	MimeGSuiteDoc string = "application/vnd.google-apps.document"
 	// MimeGSuiteFolder is mime-types of the folder on Google Drive.
