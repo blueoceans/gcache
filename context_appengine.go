2b086d96a5974b71e8cb7b87c0a7aa9b1eaabe4e
ENDOH takanao <djmchl@gmail.com>
2017-07-18 05:40:48 +0800

    set 60 sec to timeout on App Engine

--- context_appengine.go
+++ context_appengine.go
@@ -4,14 +4,19 @@ package gcache
 
 import (
 	"net/http"
+	"time"
 
 	"golang.org/x/net/context"
 	newappengine "google.golang.org/appengine"
 )
 
+const (
+	deadline = time.Duration(60) * time.Second
+)
+
 func newContext(
 	r *http.Request,
 ) *context.Context {
-	ctx := newappengine.NewContext(r)
+	ctx, _ := context.WithTimeout(newappengine.NewContext(r), deadline)
 	return &ctx
 }
