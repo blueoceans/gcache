8dd1a810fcaedf641ee3b81f3e36ea9f014a86c3
ENDOH takanao <djmchl@gmail.com>
2017-07-24 12:08:18 +0800

    refactor

--- error.go
+++ error.go
@@ -26,9 +26,9 @@ const (
 )
 
 var (
-	errInvalidSecurityTicket = []string{"invalid security ticket"}
 	errDeadlineExceeded      = []string{"Deadline exceeded"}
 	errFileNotExportable     = []string{"fileNotExportable"}
+	errInvalidSecurityTicket = []string{"invalid security ticket"}
 	errServerError           = []string{
 		"500 Internal Server Error",
 		"502 Bad Gateway",
